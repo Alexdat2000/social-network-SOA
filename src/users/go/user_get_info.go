f188267689a0d4dda05008a89c69d40bb24645d0
Alexey <alexdat2000@gmail.com>
2025-03-02 18:15:35 +0300

    Fix user getter

--- src/users/go/user_get_info.go
+++ src/users/go/user_get_info.go
@@ -1,7 +1,9 @@
 package swagger
 
 import (
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,26 +16,48 @@ func UsersGet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	req := fmt.Sprintf(`select * from users where username='%s'`, user)
-	rows, err := DB.Query(req)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
+	req := fmt.Sprintf(`select id, username, email, first_name, last_name, date_of_birth, phone_number from users where username='%s'`, user)
+	row := DB.QueryRow(req)
 
-	ans := Profile{}
+	nullableAnswer := struct {
+		Id          int32
+		Username    string
+		Email       string
+		FirstName   sql.NullString
+		LastName    sql.NullString
+		DateOfBirth sql.NullString
+		PhoneNumber sql.NullString
+	}{}
 
-	for rows.Next() {
-		err := rows.Scan(&ans.Id, &ans.Username, &ans.Email, &ans.FirstName, &ans.LastName, &ans.DateOfBirth, &ans.PhoneNumber)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	}
-	if ans.Username == "" {
+	err := row.Scan(&nullableAnswer.Id, &nullableAnswer.Username, &nullableAnswer.Email,
+		&nullableAnswer.FirstName, &nullableAnswer.LastName, &nullableAnswer.DateOfBirth,
+		&nullableAnswer.PhoneNumber)
+	if errors.Is(err, sql.ErrNoRows) {
 		w.WriteHeader(http.StatusNotFound)
 		return
+	} else if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Error when reading row: %v", err)
+		return
+	}
+
+	ans := Profile{
+		Id:       nullableAnswer.Id,
+		Username: nullableAnswer.Username,
+		Email:    nullableAnswer.Email,
+	}
+
+	if nullableAnswer.FirstName.Valid {
+		ans.FirstName = nullableAnswer.FirstName.String
+	}
+	if nullableAnswer.LastName.Valid {
+		ans.FirstName = nullableAnswer.LastName.String
+	}
+	if nullableAnswer.DateOfBirth.Valid {
+		ans.FirstName = nullableAnswer.DateOfBirth.String
+	}
+	if nullableAnswer.PhoneNumber.Valid {
+		ans.FirstName = nullableAnswer.PhoneNumber.String
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
