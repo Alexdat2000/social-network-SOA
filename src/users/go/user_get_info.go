80430a61ff0452cc190fd935dbef47474c3a8dab
Alexey <alexdat2000@gmail.com>
2025-03-02 17:48:12 +0300

    Update scheme and implement user getter

--- src/users/go/user_get_info.go
+++ src/users/go/user_get_info.go
@@ -1,8 +1,43 @@
 package swagger
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func UsersGet(w http.ResponseWriter, r *http.Request) {
+	user := r.URL.Query().Get("username")
+	if user == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	req := fmt.Sprintf(`select * from users where username='%s'`, user)
+	rows, err := DB.Query(req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	ans := Profile{}
+
+	for rows.Next() {
+		err := rows.Scan(&ans.Id, &ans.Username, &ans.Email, &ans.FirstName, &ans.LastName, &ans.DateOfBirth, &ans.PhoneNumber)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+	if ans.Username == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	res, _ := json.Marshal(ans)
+	fmt.Fprint(w, string(res))
 }
